216e7466fc8d538f14e6dc5e72101dc5e13323cc
hyojin425 <gywls5184@gmail.com>
2025-11-02 04:53:39 +0900

    feat(Validator): 구입 금액 검증 추가
    - 구입 금액이 1000원으로 나누어 떨어지지 않는 경우 예외 처리

---
+++ src/main/java/lotto/utils/Validator.java
@@ -0,0 +1,10 @@
+package lotto.utils;
+
+public class Validator {
+
+    public static void validatePurchaseAmount(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+        }
+    }
+}
