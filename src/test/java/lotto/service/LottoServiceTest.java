b4a188ea3f78550f128f0089a5bbc04d64280126
hyojin425 <gywls5184@gmail.com>
2025-11-02 06:19:49 +0900

    test(LottoServiceTest): LottoService 단위 테스트 코드 추가
    - 구입 금액에 해당하는 수와 발행한 로또 갯수 확인
    - 구입 금액이 1000원 단위가 아닌 경우 예외 처리 확인

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,42 @@
+package lotto.service;
+
+import lotto.dto.LottoDto;
+import lotto.utils.Validator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class LottoServiceTest {
+    @DisplayName("구입 금액에 해당하는 만큼 로또를 발행한다.")
+    @Test
+    void 구입_금액에_해당하는_만큼_로또를_발행한다() {
+        // given
+        Validator validator = new Validator();
+        LottoService lottoService = new LottoService(validator);
+        int purchaseAmount = 3000;
+
+        // when
+        List<LottoDto> lottos = lottoService.buyLottos(3000);
+
+        // then
+        assertThat(lottos.size()).isEqualTo(purchaseAmount / 1000);
+    }
+
+    @DisplayName("구입 금액이 1000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void 구입_금액이_1000원_단위가_아니면_예외발생() {
+        // given
+        Validator validator = new Validator();
+        LottoService lottoService = new LottoService(validator);
+        int invalidPurchaseAmount = 2500;
+
+        // when & then
+        assertThatThrownBy(() -> lottoService.buyLottos(invalidPurchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1000원 단위");
+    }
+}
