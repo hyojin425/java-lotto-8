4ada0f49227679c8744832052ec33690c7491e8d
hyojin425 <gywls5184@gmail.com>
2025-11-01 19:49:49 +0900

    test(LottoTest): Lotto 단위 테스트 추가
    - 로또 번호가 1~45 사이 정수가 아닐 때 예외 확인하는 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -22,5 +22,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호에 1~45 사이의 정수가 아닌 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_1_45_사이의_정수가_아닌_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
