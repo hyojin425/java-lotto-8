a0385ad68e144979a41012bf55e06da3974ea9ab
hyojin425 <gywls5184@gmail.com>
2025-11-02 00:18:41 +0900

    feat(LottoResult): 당첨 로또 번호와 보너스 번호 검증 추가
    - 당첨 로또 번호에 보너스 번호와 중복된 숫자가 있을 경우 예외 처리
    - 보너스 로또 번호가 1~45 사이의 정수가 아닌 경우 예외 처리

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoResult {
+    private final Lotto winningLotto;
+    private final Integer bonusNumber;
+
+    public LottoResult(List<Integer> numbers, Integer bonusNumber) {
+        Lotto winningLotto = new Lotto(numbers);
+        bonusNumberValidate(numbers, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void bonusNumberValidate(List<Integer> numbers, Integer bonusNumber) {
+        if (!isBonusNumberNotDuplicated(numbers, bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 로또 번호에 보너스 번호와 중복된 숫자가 있습니다.");
+        }
+호
+        if (!isBonusNumberValidNumberRange(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 로또 번호는 1~45 사이의 정수입니다.");
+        }
+    }
+
+    public boolean isBonusNumberNotDuplicated(List<Integer> numbers, Integer bonusNumber) {
+        return !numbers.contains(bonusNumber);
+    }
+
+    public boolean isBonusNumberValidNumberRange(Integer bonusNumber) {
+        return bonusNumber >= 1 && bonusNumber <= 45;
+    }
+}
