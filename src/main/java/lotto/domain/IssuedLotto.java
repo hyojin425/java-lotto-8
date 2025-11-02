454960b651d15a9ed7aa1d4d9e9f44358b2962e2
hyojin425 <gywls5184@gmail.com>
2025-11-02 19:35:06 +0900

    feat(IssuedLotto): 각 등수의 개수 구하기 추가

--- src/main/java/lotto/domain/IssuedLotto.java
+++ src/main/java/lotto/domain/IssuedLotto.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class IssuedLotto {
 
@@ -10,5 +14,12 @@ public class IssuedLotto {
     public IssuedLotto(List<Lotto> lottos, int purchasedAmount) {
         this.lottos = lottos;
         this.purchasedAmount = purchasedAmount;
+    }
+
+    public Map<LottoRank, Long> rankCount(LottoResult lottoResult) {
+        return lottos.stream()
+                .map(lotto -> lotto.calculateRank(lottoResult))
+                .filter(Objects::nonNull)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 }
