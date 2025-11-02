e617927f7e74bfe00a7c4769cc0d4abe8392abf9
hyojin425 <gywls5184@gmail.com>
2025-11-03 02:04:31 +0900

    fix(LottoRankDto): 보너스 번호 일치 여부 추가
    - 로또 결과 출력시 필요

--- src/main/java/lotto/dto/LottoRankDto.java
+++ src/main/java/lotto/dto/LottoRankDto.java
@@ -2,9 +2,9 @@ package lotto.dto;
 
 import lotto.domain.LottoRank;
 
-public record LottoRankDto(int matchCount, int prize, int totalCount) {
+public record LottoRankDto(int matchCount, int prize, boolean bonusMatch, int totalCount) {
 
     public static LottoRankDto of(LottoRank rank, int totalCount) {
-        return new LottoRankDto(rank.getMatchCount(), rank.getPrize(), totalCount);
+        return new LottoRankDto(rank.getMatchCount(), rank.getPrize(), rank.isRequiresBonus(), totalCount);
     }
 }
