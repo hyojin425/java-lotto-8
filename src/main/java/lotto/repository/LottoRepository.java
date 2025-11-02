c83e4c059f46686820a2d2cb7a40b59dc243845b
hyojin425 <gywls5184@gmail.com>
2025-11-02 17:31:11 +0900

    feat(LottoRepository): LottoRepository 추가

---
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -0,0 +1,20 @@
+package lotto.repository;
+
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoRepository {
+
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public void save(Lotto lotto) {
+        lottos.add(lotto);
+    }
+
+    public List<Lotto> findAll() {
+        return Collections.unmodifiableList(lottos);
+    }
+}
