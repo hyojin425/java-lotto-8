dafc50cb545014da3e5a9a62b60fc435a7279d8a
hyojin425 <gywls5184@gmail.com>
2025-11-02 17:14:10 +0900

    test(LottoTest): 로또 등수 계산 확인 테스트 코드 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @Test
@@ -26,5 +27,75 @@ class LottoTest {
     void 로또_번호에_1_45_사이의_정수가_아닌_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1,2,3,4,5,6 이고 당첨 번호가 1,2,3,7,8,9 이고 보너스 번호가 10일 경우 5등이다.")
+    @Test
+    void 로또_계산_테스트_5등() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoResult lottoResult = new LottoResult(List.of(1, 2, 3, 7, 8, 9), 10);
+
+        // when
+        LottoRank rank = lotto.calculateRank(lottoResult);
+
+        // then
+        assertThat(rank).isEqualTo(LottoRank.FIFTH);
+    }
+
+    @DisplayName("로또 번호가 1,2,3,4,5,6 이고 당첨 번호가 1,2,3,4,8,9 이고 보너스 번호가 10일 경우 4등이다.")
+    @Test
+    void 로또_계산_테스트_4등() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoResult lottoResult = new LottoResult(List.of(1, 2, 3, 4, 8, 9), 10);
+
+        // when
+        LottoRank rank = lotto.calculateRank(lottoResult);
+
+        // then
+        assertThat(rank).isEqualTo(LottoRank.FOURTH);
+    }
+
+    @DisplayName("로또 번호가 1,2,3,4,5,6 이고 당첨 번호가 1,2,3,4,5,9 이고 보너스 번호가 10일 경우 3등이다.")
+    @Test
+    void 로또_계산_테스트_3등() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoResult lottoResult = new LottoResult(List.of(1, 2, 3, 4, 5, 9), 10);
+
+        // when
+        LottoRank rank = lotto.calculateRank(lottoResult);
+
+        // then
+        assertThat(rank).isEqualTo(LottoRank.THIRD);
+    }
+
+    @DisplayName("로또 번호가 1,2,3,4,5,6 이고 당첨 번호가 1,2,3,4,5,9 이고 보너스 번호가 6일 경우 2등이다.")
+    @Test
+    void 로또_계산_테스트_2등() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoResult lottoResult = new LottoResult(List.of(1, 2, 3, 4, 5, 9), 6);
+
+        // when
+        LottoRank rank = lotto.calculateRank(lottoResult);
+
+        // then
+        assertThat(rank).isEqualTo(LottoRank.SECOND);
+    }
+
+    @DisplayName("로또 번호가 1,2,3,4,5,6 이고 당첨 번호가 1,2,3,4,5,6 이고 보너스 번호가 7일 경우 2등이다.")
+    @Test
+    void 로또_계산_테스트_1등() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoResult lottoResult = new LottoResult(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        // when
+        LottoRank rank = lotto.calculateRank(lottoResult);
+
+        // then
+        assertThat(rank).isEqualTo(LottoRank.FIRST);
     }
 }
