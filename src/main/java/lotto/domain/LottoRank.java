f8054eafec219e219fff4ff5e38c1272e0147c00
hyojin425 <gywls5184@gmail.com>
2025-11-02 18:56:33 +0900

    fix(LottoRank): NONE 제거

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -6,8 +6,8 @@ public enum LottoRank {
     THIRD(5, false, 1_500_000),
     FOURTH(4, false, 50_000),
     FIFTH(3, false, 5_000),
-    NONE(0, false, 0)
     ;
+
     private final int matchCount;
     private final boolean requiresBonus;
     private final int prize;
@@ -25,6 +25,6 @@ public enum LottoRank {
                 return rank;
             }
         }
-        return NONE;
+        return null;
     }
 }
