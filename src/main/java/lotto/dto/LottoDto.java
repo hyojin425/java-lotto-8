c9c529d6e6bece22cbb630a01d4d83f25061fb69
hyojin425 <gywls5184@gmail.com>
2025-11-02 18:33:34 +0900

    fix(LottoDto): 구매 수량 제거

--- src/main/java/lotto/dto/LottoDto.java
+++ src/main/java/lotto/dto/LottoDto.java
@@ -4,8 +4,8 @@ import lotto.domain.Lotto;
 
 import java.util.List;
 
-public record LottoDto(List<Integer> numbers, int amount) {
-    public static LottoDto from(Lotto lotto, int amount) {
-        return new LottoDto(lotto.getNumbers(), amount);
+public record LottoDto(List<Integer> numbers) {
+    public static LottoDto from(Lotto lotto) {
+        return new LottoDto(lotto.getNumbers());
     }
 }
